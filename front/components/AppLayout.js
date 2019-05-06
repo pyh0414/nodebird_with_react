53c677efa9491e1af0b9ecbea42364efde2e82e9
yeonho Park <whwlsvy12@naver.com>
2019-05-07 01:10:12 +0900

    add initial

---
+++ front/components/AppLayout.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import { Menu, Input, Button } from "antd";
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="home">
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="mail">
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+      </Menu>
+      {children}
+      <Button>회원가입</Button>
+    </div>
+  );
+};
+
+export default AppLayout;
