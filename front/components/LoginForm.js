106eaf8edf0e89a42de619987aa45107dc254582
yeonho Park <whwlsvy12@naver.com>
2019-05-13 01:08:08 +0900

    add LoginForm

---
+++ front/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { useCallback } from "react";
+import { Button, Form } from "antd";
+import Link from "next/link";
+
+import { useInput } from "../pages/signup";
+
+const LoginForm = () => {
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const onSubmitForm = useCallback(
+    e => {
+      e.preventDefault();
+    },
+    [id, password]
+  );
+  return (
+    <Form onSubmit={onSubmitForm}>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <input
+          name="user-password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <div>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </div>
+    </Form>
+  );
+};
+export default LoginForm;
