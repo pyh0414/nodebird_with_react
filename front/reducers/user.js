5abbf6abbc45b8a90960f3ee24370f04b7dde747
yeonho Park <whwlsvy12@naver.com>
2019-05-19 07:28:01 +0900

    modify action

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,73 +1,115 @@
 const dummyUser = {
-  nickname: "박연호",
+  nickname: "제로초",
   Post: [],
   Followings: [],
-  Follower: []
+  Followers: [],
+  id: 1
 };
 
-export const intialState = {
-  isLoggedIn: false,
-  user: null,
-  signUpdata: {},
-  loginData: {}
+export const initialState = {
+  isLoggedIn: false, // 로그인 여부
+  isLoggingOut: false, // 로그아웃 시도중
+  isLoggingIn: false, // 로그인 시도중
+  logInErrorReason: "", // 로그인 실패 사유
+  signedUp: false, // 회원가입 성공
+  isSigningUp: false, // 회원가입 시도중
+  signUpErrorReason: "", // 회원가입 실패 사유
+  me: null, // 내 정보
+  followingList: [], // 팔로잉 리스트
+  followerList: [], // 팔로워 리스트
+  userInfo: null // 남의 정보
 };
 
-export const LOG_IN = "LOG_IN"; // 액션 이름
-export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
-export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
-export const LOG_OUT = "LOG_OUT";
-
-export const SIGN_UP = "SIGN_UP";
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 
-export const logoutAction = {
-  // 실제 액션
-  type: LOG_OUT
-};
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST"; // 액션의 이름
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"; // 액션의 이름
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE"; // 액션의 이름
 
-export const loginAction = data => {
-  // 그냥 객체인 경우는 데이터가 없거나, 데이터가 정적인 경우
-  // 실제 액션
-  return {
-    type: LOG_IN,
-    data
-  };
-};
+export const LOAD_USER_REQUEST = "LOAD_USER_REQUEST";
+export const LOAD_USER_SUCCESS = "LOAD_USER_SUCCESS";
+export const LOAD_USER_FAILURE = "LOAD_USER_FAILURE";
 
-export const signUpAction = data => {
-  // 동적인 데이터가 들어 올 경우는 함수로
-  // 실제 액션
-  return {
-    type: SIGN_UP,
-    data
-  };
-};
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-export const signUpSuccess = {
-  type: SIGN_UP_SUCCESS
-};
-const reducer = (state = intialState, action) => {
-  // state와 action을 받아서 다음 state를 생성
+export const LOAD_FOLLOW_REQUEST = "LOAD_FOLLOW_REQUEST";
+export const LOAD_FOLLOW_SUCCESS = "LOAD_FOLLOW_SUCCESS";
+export const LOAD_FOLLOW_FAILURE = "LOAD_FOLLOW_FAILURE";
 
+export const FOLLOW_USER_REQUEST = "FOLLOW_USER_REQUEST";
+export const FOLLOW_USER_SUCCESS = "FOLLOW_USER_SUCCESS";
+export const FOLLOW_USER_FAILURE = "FOLLOW_USER_FAILURE";
+
+export const UNFOLLOW_USER_REQUEST = "UNFOLLOW_USER_REQUEST";
+export const UNFOLLOW_USER_SUCCESS = "UNFOLLOW_USER_SUCCESS";
+export const UNFOLLOW_USER_FAILURE = "UNFOLLOW_USER_FAILURE";
+
+export const REMOVE_FOLLOWER_REQUEST = "REMOVE_FOLLOWER_REQUEST";
+export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
+export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
+
+export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case LOG_IN:
+    case LOG_IN_REQUEST: {
       return {
         ...state,
-        isLoggedIn: true,
-        user: dummyUser,
-        loginData: action.data
+        isLoggingIn: true,
+        logInErrorReason: ""
       };
-    case LOG_OUT:
+    }
+    case LOG_IN_SUCCESS: {
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        me: dummyUser,
+        isLoading: false
+      };
+    }
+    case LOG_IN_FAILURE: {
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+        logInErrorReason: action.error,
+        me: null
+      };
+    }
+    case LOG_OUT_REQUEST: {
       return {
         ...state,
         isLoggedIn: false,
-        user: null
+        me: null
       };
-    case SIGN_UP:
+    }
+    case SIGN_UP_REQUEST: {
       return {
         ...state,
-        signupData: action.data
+        isSigningUp: true,
+        isSignedUp: false,
+        signUpErrorReason: ""
       };
+    }
+    case SIGN_UP_SUCCESS: {
+      return {
+        ...state,
+        isSigningUp: false,
+        isSignedUp: true
+      };
+    }
+    case SIGN_UP_FAILURE: {
+      return {
+        ...state,
+        isSigningUp: false,
+        signUpErrorReason: action.error
+      };
+    }
     default: {
       return {
         ...state
@@ -75,5 +117,3 @@ const reducer = (state = intialState, action) => {
     }
   }
 };
-
-export default reducer;
