ee34a57fb1305dc3616ffb8ca124ff8d203b1a33
yeonho Park <whwlsvy12@naver.com>
2019-05-25 16:18:55 +0900

    Modify: load post

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
+import { LOAD_MAIN_POSTS_REQUEST } from "../reducers/post";
 
 const Home = () => {
-  const { isLoggedIn } = useSelector(state => state.user); // user가 변경되면 Home컴포넌트도 re-rendering되기 떄문에, 최대한 나눠줘야함
+  const dispatch = useDispatch();
+  const { me } = useSelector(state => state.user); // user가 변경되면 Home컴포넌트도 re-rendering되기 떄문에, 최대한 나눠줘야함
   const { mainPosts } = useSelector(state => state.post);
-
+  useEffect(() => {
+    dispatch({
+      type: LOAD_MAIN_POSTS_REQUEST
+    });
+  }, []);
   return (
     <div>
-      {isLoggedIn && <PostForm />}
+      {me && <PostForm />}
       {mainPosts.map(c => {
         return <PostCard key={c} post={c} />;
       })}
