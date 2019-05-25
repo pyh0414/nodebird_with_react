6fbf7bb332c5792c0ac01a9c32da00b59b962c83
yeonho Park <whwlsvy12@naver.com>
2019-05-25 16:21:26 +0900

    Modify: add hashtags link

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -56,7 +56,21 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={
+            <div>
+              {post.content.split(/(#[^\s]+)/g).map(v => {
+                if (v.match(/#[^\s]+/)) {
+                  return (
+                    <Link href="/hashtag/">
+                      <a>{v}</a>
+                    </Link>
+                  );
+                } else {
+                  return v;
+                }
+              })}
+            </div>
+          }
         />
       </Card>
       {commentFormOpened && (
