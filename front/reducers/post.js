7e2bcb0875b92b782695ea4208625f3f69d51afb
yeonho Park <whwlsvy12@naver.com>
2019-05-29 16:41:21 +0900

    Apply: immer

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,274 +1,210 @@
+import produce from "immer";
+
 export const initialState = {
-  mainPosts: [
-    {
-      id: 1,
-      User: {
-        id: 1,
-        nickname: "제로초"
-      },
-      content: "첫 번째 게시글",
-      img:
-        "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726",
-      Comments: []
-    }
-  ], // 화면에 보일 포스트들
+  mainPosts: [], // 화면에 보일 포스트들
   imagePaths: [], // 미리보기 이미지 경로
   addPostErrorReason: "", // 포스트 업로드 실패 사유
   isAddingPost: false, // 포스트 업로드 중
   postAdded: false, // 포스트 업로드 성공
   isAddingComment: false,
   addCommentErrorReason: "",
-  commentAdded: false
+  commentAdded: false,
+  singlePost: null
 };
 
-export const LOAD_MAIN_POSTS_REQUEST = "POST/LOAD_MAIN_POSTS_REQUEST";
-export const LOAD_MAIN_POSTS_SUCCESS = "POST/LOAD_MAIN_POSTS_SUCCESS";
-export const LOAD_MAIN_POSTS_FAILURE = "POST/LOAD_MAIN_POSTS_FAILURE";
+export const LOAD_MAIN_POSTS_REQUEST = "LOAD_MAIN_POSTS_REQUEST";
+export const LOAD_MAIN_POSTS_SUCCESS = "LOAD_MAIN_POSTS_SUCCESS";
+export const LOAD_MAIN_POSTS_FAILURE = "LOAD_MAIN_POSTS_FAILURE";
 
-export const LOAD_HASHTAG_POSTS_REQUEST = "POST/LOAD_HASHTAG_POSTS_REQUEST";
-export const LOAD_HASHTAG_POSTS_SUCCESS = "POST/LOAD_HASHTAG_POSTS_SUCCESS";
-export const LOAD_HASHTAG_POSTS_FAILURE = "POST/LOAD_HASHTAG_POSTS_FAILURE";
+export const LOAD_HASHTAG_POSTS_REQUEST = "LOAD_HASHTAG_POSTS_REQUEST";
+export const LOAD_HASHTAG_POSTS_SUCCESS = "LOAD_HASHTAG_POSTS_SUCCESS";
+export const LOAD_HASHTAG_POSTS_FAILURE = "LOAD_HASHTAG_POSTS_FAILURE";
 
-export const LOAD_USER_POSTS_REQUEST = "POST/LOAD_USER_POSTS_REQUEST";
-export const LOAD_USER_POSTS_SUCCESS = "POST/LOAD_USER_POSTS_SUCCESS";
-export const LOAD_USER_POSTS_FAILURE = "POST/LOAD_USER_POSTS_FAILURE";
+export const LOAD_USER_POSTS_REQUEST = "LOAD_USER_POSTS_REQUEST";
+export const LOAD_USER_POSTS_SUCCESS = "LOAD_USER_POSTS_SUCCESS";
+export const LOAD_USER_POSTS_FAILURE = "LOAD_USER_POSTS_FAILURE";
 
-export const UPLOAD_IMAGES_REQUEST = "POST/UPLOAD_IMAGES_REQUEST";
-export const UPLOAD_IMAGES_SUCCESS = "POST/UPLOAD_IMAGES_SUCCESS";
-export const UPLOAD_IMAGES_FAILURE = "POST/UPLOAD_IMAGES_FAILURE";
+export const UPLOAD_IMAGES_REQUEST = "UPLOAD_IMAGES_REQUEST";
+export const UPLOAD_IMAGES_SUCCESS = "UPLOAD_IMAGES_SUCCESS";
+export const UPLOAD_IMAGES_FAILURE = "UPLOAD_IMAGES_FAILURE";
 
-export const REMOVE_IMAGE = "POST/REMOVE_IMAGE";
+export const REMOVE_IMAGE = "REMOVE_IMAGE";
 
-export const ADD_POST_REQUEST = "POST/ADD_POST_REQUEST";
-export const ADD_POST_SUCCESS = "POST/ADD_POST_SUCCESS";
-export const ADD_POST_FAILURE = "POST/ADD_POST_FAILURE";
+export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
+export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
+export const ADD_POST_FAILURE = "ADD_POST_FAILURE";
 
-export const LIKE_POST_REQUEST = "POST/LIKE_POST_REQUEST";
-export const LIKE_POST_SUCCESS = "POST/LIKE_POST_SUCCESS";
-export const LIKE_POST_FAILURE = "POST/LIKE_POST_FAILURE";
+export const LIKE_POST_REQUEST = "LIKE_POST_REQUEST";
+export const LIKE_POST_SUCCESS = "LIKE_POST_SUCCESS";
+export const LIKE_POST_FAILURE = "LIKE_POST_FAILURE";
 
-export const UNLIKE_POST_REQUEST = "POST/UNLIKE_POST_REQUEST";
-export const UNLIKE_POST_SUCCESS = "POST/UNLIKE_POST_SUCCESS";
-export const UNLIKE_POST_FAILURE = "POST/UNLIKE_POST_FAILURE";
+export const UNLIKE_POST_REQUEST = "UNLIKE_POST_REQUEST";
+export const UNLIKE_POST_SUCCESS = "UNLIKE_POST_SUCCESS";
+export const UNLIKE_POST_FAILURE = "UNLIKE_POST_FAILURE";
 
-export const ADD_COMMENT_REQUEST = "POST/ADD_COMMENT_REQUEST";
-export const ADD_COMMENT_SUCCESS = "POST/ADD_COMMENT_SUCCESS";
-export const ADD_COMMENT_FAILURE = "POST/ADD_COMMENT_FAILURE";
+export const ADD_COMMENT_REQUEST = "ADD_COMMENT_REQUEST";
+export const ADD_COMMENT_SUCCESS = "ADD_COMMENT_SUCCESS";
+export const ADD_COMMENT_FAILURE = "ADD_COMMENT_FAILURE";
 
-export const LOAD_COMMENTS_REQUEST = "POST/LOAD_COMMENTS_REQUEST";
-export const LOAD_COMMENTS_SUCCESS = "POST/LOAD_COMMENTS_SUCCESS";
-export const LOAD_COMMENTS_FAILURE = "POST/LOAD_COMMENTS_FAILURE";
+export const LOAD_COMMENTS_REQUEST = "LOAD_COMMENTS_REQUEST";
+export const LOAD_COMMENTS_SUCCESS = "LOAD_COMMENTS_SUCCESS";
+export const LOAD_COMMENTS_FAILURE = "LOAD_COMMENTS_FAILURE";
 
-export const RETWEET_REQUEST = "POST/RETWEET_REQUEST";
-export const RETWEET_SUCCESS = "POST/RETWEET_SUCCESS";
-export const RETWEET_FAILURE = "POST/RETWEET_FAILURE";
+export const RETWEET_REQUEST = "RETWEET_REQUEST";
+export const RETWEET_SUCCESS = "RETWEET_SUCCESS";
+export const RETWEET_FAILURE = "RETWEET_FAILURE";
 
-export const REMOVE_POST_REQUEST = "POST/REMOVE_POST_REQUEST";
-export const REMOVE_POST_SUCCESS = "POST/REMOVE_POST_SUCCESS";
-export const REMOVE_POST_FAILURE = "POST/REMOVE_POST_FAILURE";
+export const REMOVE_POST_REQUEST = "REMOVE_POST_REQUEST";
+export const REMOVE_POST_SUCCESS = "REMOVE_POST_SUCCESS";
+export const REMOVE_POST_FAILURE = "REMOVE_POST_FAILURE";
+
+export const LOAD_POST_REQUEST = "LOAD_POST_REQUEST";
+export const LOAD_POST_SUCCESS = "LOAD_POST_SUCCESS";
+export const LOAD_POST_FAILURE = "LOAD_POST_FAILURE";
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST: {
-      return {
-        ...state,
-        isAddingPost: true,
-        addPostErrorReason: "",
-        postAdded: false
-      };
+  return produce(state, draft => {
+    switch (action.type) {
+      case UPLOAD_IMAGES_REQUEST: {
+        break;
+      }
+      case UPLOAD_IMAGES_SUCCESS: {
+        action.data.forEach(p => {
+          draft.imagePaths.push(p);
+        });
+        break;
+      }
+      case UPLOAD_IMAGES_FAILURE: {
+        break;
+      }
+      case REMOVE_IMAGE: {
+        const index = draft.imagePaths.findIndex((v, i) => i === action.index);
+        draft.imagePaths.splice(index, 1);
+        break;
+      }
+      case ADD_POST_REQUEST: {
+        draft.isAddingPost = true;
+        draft.addingPostErrorReason = "";
+        draft.postAdded = false;
+        break;
+      }
+      case ADD_POST_SUCCESS: {
+        draft.isAddingPost = false;
+        draft.mainPosts.unshift(action.data);
+        draft.postAdded = true;
+        draft.imagePaths = [];
+        break;
+      }
+      case ADD_POST_FAILURE: {
+        draft.isAddingPost = false;
+        draft.addPostErrorReason = action.error;
+        break;
+      }
+      case ADD_COMMENT_REQUEST: {
+        draft.isAddingComment = true;
+        draft.addCommentErrorReason = "";
+        draft.commentAdded = false;
+        break;
+      }
+      case ADD_COMMENT_SUCCESS: {
+        const postIndex = draft.mainPosts.findIndex(
+          v => v.id === action.data.postId
+        );
+        draft.mainPosts[postIndex].Comments.push(action.data.comment);
+        draft.isAddingComment = false;
+        draft.commentAdded = true;
+        break;
+      }
+      case ADD_COMMENT_FAILURE: {
+        draft.isAddingComment = false;
+        draft.addingPostErrorReason = action.error;
+        break;
+      }
+      case LOAD_COMMENTS_SUCCESS: {
+        const postIndex = draft.mainPosts.findIndex(
+          v => v.id === action.data.postId
+        );
+        draft.mainPosts[postIndex].Comments = action.data.comments;
+        break;
+      }
+      case LOAD_MAIN_POSTS_REQUEST:
+      case LOAD_HASHTAG_POSTS_REQUEST:
+      case LOAD_USER_POSTS_REQUEST: {
+        draft.mainPosts = !action.lastId ? [] : draft.mainPosts;
+        draft.hasMorePost = action.lastId ? draft.hasMorePost : true;
+        break;
+      }
+      case LOAD_MAIN_POSTS_SUCCESS:
+      case LOAD_HASHTAG_POSTS_SUCCESS:
+      case LOAD_USER_POSTS_SUCCESS: {
+        action.data.forEach(d => {
+          draft.mainPosts.push(d);
+        });
+        draft.hasMorePost = action.data.length === 10;
+        break;
+      }
+      case LOAD_MAIN_POSTS_FAILURE:
+      case LOAD_HASHTAG_POSTS_FAILURE:
+      case LOAD_USER_POSTS_FAILURE: {
+        break;
+      }
+      case LIKE_POST_REQUEST: {
+        break;
+      }
+      case LIKE_POST_SUCCESS: {
+        const postIndex = draft.mainPosts.findIndex(
+          v => v.id === action.data.postId
+        );
+        draft.mainPosts[postIndex].Likers.unshift({ id: action.data.userId });
+        break;
+      }
+      case LIKE_POST_FAILURE: {
+        break;
+      }
+      case UNLIKE_POST_REQUEST: {
+        break;
+      }
+      case UNLIKE_POST_SUCCESS: {
+        const postIndex = draft.mainPosts.findIndex(
+          v => v.id === action.data.postId
+        );
+        const likeIndex = draft.mainPosts[postIndex].Likers.findIndex(
+          v => v.id === action.data.userId
+        );
+        draft.mainPosts[postIndex].Likers.splice(likeIndex, 1);
+        break;
+      }
+      case UNLIKE_POST_FAILURE: {
+        break;
+      }
+      case RETWEET_REQUEST: {
+        break;
+      }
+      case RETWEET_SUCCESS: {
+        draft.mainPosts.unshift(action.data);
+        break;
+      }
+      case RETWEET_FAILURE: {
+        break;
+      }
+      case REMOVE_POST_REQUEST: {
+        break;
+      }
+      case REMOVE_POST_SUCCESS: {
+        const index = draft.mainPosts.findIndex(v => v.id === action.data);
+        draft.mainPosts.splice(index, 1);
+        break;
+      }
+      case REMOVE_POST_FAILURE: {
+        break;
+      }
+      case LOAD_POST_SUCCESS: {
+        draft.singlePost = action.data;
+        break;
+      }
+      default: {
+        break;
+      }
     }
-    case ADD_POST_SUCCESS: {
-      return {
-        ...state,
-        isAddingPost: false,
-        mainPosts: [action.data, ...state.mainPosts],
-        postAdded: true,
-        imagePaths: []
-      };
-    }
-    case ADD_POST_FAILURE: {
-      return {
-        ...state,
-        isAddingPost: false,
-        addPostErrorReason: action.error
-      };
-    }
-    case LOAD_COMMENTS_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(
-        v => v.id === action.data.postId
-      );
-      const post = state.mainPosts[postIndex];
-      const Comments = action.data.comments;
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Comments };
-      return {
-        ...state,
-        mainPosts
-      };
-    }
-
-    case LOAD_MAIN_POSTS_REQUEST:
-    case LOAD_HASHTAG_POSTS_REQUEST:
-    case LOAD_USER_POSTS_REQUEST: {
-      return {
-        ...state,
-        mainPosts: []
-      };
-    }
-
-    case LOAD_MAIN_POSTS_SUCCESS:
-    case LOAD_HASHTAG_POSTS_SUCCESS:
-    case LOAD_USER_POSTS_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: action.data
-      };
-    }
-    case LOAD_MAIN_POSTS_FAILURE:
-    case LOAD_HASHTAG_POSTS_FAILURE:
-    case LOAD_USER_POSTS_FAILURE: {
-      return {
-        ...state,
-        mainPosts: []
-      };
-    }
-    case ADD_COMMENT_REQUEST: {
-      return {
-        ...state,
-        isAddingComment: true,
-        addCommentErrorReason: "",
-        commentAdded: false
-      };
-    }
-    case ADD_COMMENT_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(
-        v => v.id === action.data.postId
-      );
-      const post = state.mainPosts[postIndex];
-      const Comments = [...post.Comments, action.data.comment];
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Comments };
-      return {
-        ...state,
-        isAddingComment: false,
-        mainPosts,
-        commentAdded: true
-      };
-    }
-    case ADD_COMMENT_FAILURE: {
-      return {
-        ...state,
-        isAddingComment: false,
-        addCommentErrorReason: action.error
-      };
-    }
-    case UPLOAD_IMAGES_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case UPLOAD_IMAGES_SUCCESS: {
-      return {
-        ...state,
-        imagePaths: [...state.imagePaths, ...action.data]
-      };
-    }
-    case UPLOAD_IMAGES_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case REMOVE_IMAGE: {
-      return {
-        ...state,
-        imagePaths: state.imagePaths.filter((v, i) => i !== action.index)
-      };
-    }
-    case LIKE_POST_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case LIKE_POST_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(
-        v => v.id === action.data.postId
-      );
-      const post = state.mainPosts[postIndex];
-      const Likers = [{ id: action.data.userId }, ...post.Likers];
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Likers };
-      return {
-        ...state,
-        mainPosts
-      };
-    }
-    case LIKE_POST_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case UNLIKE_POST_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case UNLIKE_POST_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex(
-        v => v.id === action.data.postId
-      );
-      const post = state.mainPosts[postIndex];
-      const Likers = post.Likers.filter(v => v.id !== action.data.userId);
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Likers };
-      return {
-        ...state,
-        mainPosts
-      };
-    }
-    case UNLIKE_POST_FAILURE: {
-      return {
-        ...state
-      };
-    }
-
-    case RETWEET_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case RETWEET_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: [action.data, ...state.mainPosts]
-      };
-    }
-    case RETWEET_FAILURE: {
-      return {
-        ...state
-      };
-    }
-
-    case REMOVE_POST_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case REMOVE_POST_SUCCESS: {
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter(v => v.id !== action.data)
-      };
-    }
-    case REMOVE_POST_FAILURE: {
-      return {
-        ...state
-      };
-    }
-
-    default: {
-      return {
-        ...state
-      };
-    }
-  }
+  });
 };
--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,3 +1,5 @@
+import produce from "immer";
+
 export const initialState = {
   isLoggingOut: false, // 로그아웃 시도중
   isLoggingIn: false, // 로그인 시도중
@@ -10,277 +12,206 @@ export const initialState = {
   followerList: [], // 팔로워 리스트
   userInfo: null, // 남의 정보
   isEditingNickname: false, // 이름 변경 중
-  editNicknameErrorReason: "" // 이름 변경 실패 사유
+  editNicknameErrorReason: "", // 이름 변경 실패 사유
+  hasMoreFollower: false,
+  hasMoreFollowing: false
 };
 
-export const SIGN_UP_REQUEST = "USER/SIGN_UP_REQUEST";
-export const SIGN_UP_SUCCESS = "USER/SIGN_UP_SUCCESS";
-export const SIGN_UP_FAILURE = "USER/SIGN_UP_FAILURE";
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 
-export const LOG_IN_REQUEST = "USER/LOG_IN_REQUEST"; // 액션의 이름
-export const LOG_IN_SUCCESS = "USER/LOG_IN_SUCCESS"; // 액션의 이름
-export const LOG_IN_FAILURE = "USER/LOG_IN_FAILURE"; // 액션의 이름
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST"; // 액션의 이름
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"; // 액션의 이름
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE"; // 액션의 이름
 
-export const LOAD_USER_REQUEST = "USER/LOAD_USER_REQUEST";
-export const LOAD_USER_SUCCESS = "USER/LOAD_USER_SUCCESS";
-export const LOAD_USER_FAILURE = "USER/LOAD_USER_FAILURE";
+export const LOAD_USER_REQUEST = "LOAD_USER_REQUEST";
+export const LOAD_USER_SUCCESS = "LOAD_USER_SUCCESS";
+export const LOAD_USER_FAILURE = "LOAD_USER_FAILURE";
 
-export const LOG_OUT_REQUEST = "USER/LOG_OUT_REQUEST";
-export const LOG_OUT_SUCCESS = "USER/LOG_OUT_SUCCESS";
-export const LOG_OUT_FAILURE = "USER/LOG_OUT_FAILURE";
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-export const LOAD_FOLLOWERS_REQUEST = "USER/LOAD_FOLLOWERS_REQUEST";
-export const LOAD_FOLLOWERS_SUCCESS = "USER/LOAD_FOLLOWERS_SUCCESS";
-export const LOAD_FOLLOWERS_FAILURE = "USER/LOAD_FOLLOWERS_FAILURE";
+export const LOAD_FOLLOWERS_REQUEST = "LOAD_FOLLOWERS_REQUEST";
+export const LOAD_FOLLOWERS_SUCCESS = "LOAD_FOLLOWERS_SUCCESS";
+export const LOAD_FOLLOWERS_FAILURE = "LOAD_FOLLOWERS_FAILURE";
 
-export const LOAD_FOLLOWINGS_REQUEST = "USER/LOAD_FOLLOWINGS_REQUEST";
-export const LOAD_FOLLOWINGS_SUCCESS = "USER/LOAD_FOLLOWINGS_SUCCESS";
-export const LOAD_FOLLOWINGS_FAILURE = "USER/LOAD_FOLLOWINGS_FAILURE";
+export const LOAD_FOLLOWINGS_REQUEST = "LOAD_FOLLOWINGS_REQUEST";
+export const LOAD_FOLLOWINGS_SUCCESS = "LOAD_FOLLOWINGS_SUCCESS";
+export const LOAD_FOLLOWINGS_FAILURE = "LOAD_FOLLOWINGS_FAILURE";
 
-export const FOLLOW_USER_REQUEST = "USER/FOLLOW_USER_REQUEST";
-export const FOLLOW_USER_SUCCESS = "USER/FOLLOW_USER_SUCCESS";
-export const FOLLOW_USER_FAILURE = "USER/FOLLOW_USER_FAILURE";
+export const FOLLOW_USER_REQUEST = "FOLLOW_USER_REQUEST";
+export const FOLLOW_USER_SUCCESS = "FOLLOW_USER_SUCCESS";
+export const FOLLOW_USER_FAILURE = "FOLLOW_USER_FAILURE";
 
-export const UNFOLLOW_USER_REQUEST = "USER/UNFOLLOW_USER_REQUEST";
-export const UNFOLLOW_USER_SUCCESS = "USER/UNFOLLOW_USER_SUCCESS";
-export const UNFOLLOW_USER_FAILURE = "USER/UNFOLLOW_USER_FAILURE";
+export const UNFOLLOW_USER_REQUEST = "UNFOLLOW_USER_REQUEST";
+export const UNFOLLOW_USER_SUCCESS = "UNFOLLOW_USER_SUCCESS";
+export const UNFOLLOW_USER_FAILURE = "UNFOLLOW_USER_FAILURE";
 
-export const REMOVE_FOLLOWER_REQUEST = "USER/REMOVE_FOLLOWER_REQUEST";
-export const REMOVE_FOLLOWER_SUCCESS = "USER/REMOVE_FOLLOWER_SUCCESS";
-export const REMOVE_FOLLOWER_FAILURE = "USER/REMOVE_FOLLOWER_FAILURE";
+export const REMOVE_FOLLOWER_REQUEST = "REMOVE_FOLLOWER_REQUEST";
+export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
+export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
 
-export const EDIT_NICKNAME_REQUEST = "USER/EDIT_NICKNAME_REQUEST";
-export const EDIT_NICKNAME_SUCCESS = "USER/EDIT_NICKNAME_SUCCESS";
-export const EDIT_NICKNAME_FAILURE = "USER/EDIT_NICKNAME_FAILURE";
+export const EDIT_NICKNAME_REQUEST = "EDIT_NICKNAME_REQUEST";
+export const EDIT_NICKNAME_SUCCESS = "EDIT_NICKNAME_SUCCESS";
+export const EDIT_NICKNAME_FAILURE = "EDIT_NICKNAME_FAILURE";
 
-export const ADD_POST_TO_ME = "USER/ADD_POST_TO_ME";
-export const REMOVE_POST_OF_ME = "USER/REMOVE_POST_OF_ME";
+export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
+export const REMOVE_POST_OF_ME = "REMOVE_POST_OF_ME";
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case LOG_IN_REQUEST: {
-      return {
-        ...state,
-        isLoggingIn: true,
-        logInErrorReason: ""
-      };
-    }
-    case LOG_IN_SUCCESS: {
-      return {
-        ...state,
-        isLoggingIn: false,
-
-        me: action.data,
-        isLoading: false
-      };
-    }
-    case LOG_IN_FAILURE: {
-      return {
-        ...state,
-        isLoggingIn: false,
-
-        logInErrorReason: action.error,
-        me: null
-      };
-    }
-    case LOG_OUT_REQUEST: {
-      return {
-        ...state,
-        isLoggingOut: true
-      };
-    }
-    case LOG_OUT_SUCCESS: {
-      return {
-        ...state,
-        isLoggingOut: false,
-
-        me: null
-      };
-    }
-    case SIGN_UP_REQUEST: {
-      return {
-        ...state,
-        isSigningUp: true,
-        isSignedUp: false,
-        signUpErrorReason: ""
-      };
-    }
-    case SIGN_UP_SUCCESS: {
-      return {
-        ...state,
-        isSigningUp: false,
-        isSignedUp: true
-      };
-    }
-    case SIGN_UP_FAILURE: {
-      return {
-        ...state,
-        isSigningUp: false,
-        signUpErrorReason: action.error
-      };
-    }
-    case LOAD_USER_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case LOAD_USER_SUCCESS: {
-      if (action.me) {
-        return {
-          ...state,
-          me: action.data
-        };
+  return produce(state, draft => {
+    switch (action.type) {
+      case LOG_IN_REQUEST: {
+        draft.isLoggedIn = true;
+        draft.logInErrorReason = "";
+        break;
       }
-      return {
-        ...state,
-        userInfo: action.data
-      };
-    }
-    case LOAD_USER_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case FOLLOW_USER_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case FOLLOW_USER_SUCCESS: {
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Followings: [{ id: action.data }, ...state.me.Followings]
+      case LOG_IN_SUCCESS: {
+        draft.isLoggedIn = true;
+        draft.logInErrorReason = "";
+        draft.me = action.data;
+        break;
+      }
+      case LOG_IN_FAILURE: {
+        draft.isLoggedIn = false;
+        draft.logInErrorReason = action.reason;
+        draft.me = null;
+        break;
+      }
+      case LOG_OUT_REQUEST: {
+        draft.isLoggingOut = true;
+        break;
+      }
+      case LOG_OUT_SUCCESS: {
+        draft.isLoggingOut = false;
+        draft.me = null;
+        break;
+      }
+      case SIGN_UP_REQUEST: {
+        draft.isSignedUp = false;
+        draft.isSigningUp = true;
+        draft.signUpErrorReason = "";
+        break;
+      }
+      case SIGN_UP_SUCCESS: {
+        draft.isSigningUp = false;
+        draft.isSignedUp = true;
+        break;
+      }
+      case SIGN_UP_FAILURE: {
+        draft.isSigningUp = false;
+        draft.signUpErrorReason = action.error;
+        break;
+      }
+      case LOAD_USER_REQUEST: {
+        break;
+      }
+      case LOAD_USER_SUCCESS: {
+        if (action.me) {
+          draft.me = action.data;
+          break;
         }
-      };
+        draft.useInfo = action.data;
+        break;
+      }
+      case LOAD_USER_FAILURE: {
+        break;
+      }
+      case FOLLOW_USER_REQUEST: {
+        break;
+      }
+      case FOLLOW_USER_SUCCESS: {
+        draft.me.Followings.unshift({ id: action.data });
+        break;
+      }
+      case FOLLOW_USER_FAILURE: {
+        break;
+      }
+      case UNFOLLOW_USER_REQUEST: {
+        break;
+      }
+      case UNFOLLOW_USER_SUCCESS: {
+        const index = draft.me.Followings.findIndex(v => v.id === action.data);
+        draft.me.Followings.splice(index, 1);
+        const index2 = draft.followingList.findIndex(v => v.id === action.data);
+        draft.followingList.splice(index2, 1);
+        break;
+      }
+      case UNFOLLOW_USER_FAILURE: {
+        break;
+      }
+      case ADD_POST_TO_ME: {
+        draft.me.Posts.unshift({ id: action.data });
+        break;
+      }
+      case REMOVE_POST_OF_ME: {
+        const index = draft.me.Posts.findIndex(v => v.id === action.data);
+        draft.me.Posts.splice(index, 1);
+        break;
+      }
+      case LOAD_FOLLOWERS_REQUEST: {
+        draft.followerList = !action.offset ? [] : draft.followerList;
+        draft.hasMoreFollower = action.offset ? draft.hasMoreFollower : true; // 처음 데이터를 가져올 때는 더보기 버튼을 보여주는 걸로
+        break;
+      }
+      case LOAD_FOLLOWERS_SUCCESS: {
+        action.data.forEach(d => {
+          draft.followerList.push(d);
+        });
+        draft.hasMoreFollower = action.data.length === 3;
+        break;
+      }
+      case LOAD_FOLLOWERS_FAILURE: {
+        break;
+      }
+      case LOAD_FOLLOWINGS_REQUEST: {
+        draft.followingList = !action.offset ? [] : draft.followingList;
+        draft.hasMoreFollowing = action.offset ? draft.hasMoreFollowing : true; // 처음 데이터를 가져올 때는 더보기 버튼을 보여주는 걸로
+        break;
+      }
+      case LOAD_FOLLOWINGS_SUCCESS: {
+        action.data.forEach(d => {
+          draft.followingList.push(d);
+        });
+        draft.hasMoreFollowing = action.data.length === 3;
+        break;
+      }
+      case LOAD_FOLLOWINGS_FAILURE: {
+        break;
+      }
+      case REMOVE_FOLLOWER_REQUEST: {
+        break;
+      }
+      case REMOVE_FOLLOWER_SUCCESS: {
+        const index = draft.me.Followers.findIndex(v => v.id === action.data);
+        draft.me.Followers.splice(index, 1);
+        const index2 = draft.followerList.findIndex(v => v.id === action.data);
+        draft.followerList.splice(index2, 1);
+        break;
+      }
+      case REMOVE_FOLLOWER_FAILURE: {
+        break;
+      }
+      case EDIT_NICKNAME_REQUEST: {
+        draft.isEditingNickname = true;
+        draft.editNicknameErrorReason = "";
+        break;
+      }
+      case EDIT_NICKNAME_SUCCESS: {
+        draft.isEditingNickname = false;
+        draft.me.nickname = action.data;
+        break;
+      }
+      case EDIT_NICKNAME_FAILURE: {
+        draft.isEditingNickname = false;
+        draft.editNicknameErrorReason = action.error;
+        break;
+      }
+      default: {
+        break;
+      }
     }
-    case FOLLOW_USER_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case UNFOLLOW_USER_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case UNFOLLOW_USER_SUCCESS: {
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Followings: state.me.Followings.filter(v => v.id !== action.data)
-        },
-        followingList: state.followingList.filter(v => v.id !== action.data)
-      };
-    }
-    case UNFOLLOW_USER_FAILURE: {
-      return {
-        ...state
-      };
-    }
-
-    case ADD_POST_TO_ME: {
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Posts: [{ id: action.data }, ...state.me.Posts]
-        }
-      };
-    }
-    case LOAD_FOLLOWERS_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case LOAD_FOLLOWERS_SUCCESS: {
-      return {
-        ...state,
-        followerList: action.data
-      };
-    }
-    case LOAD_FOLLOWERS_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case LOAD_FOLLOWINGS_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case LOAD_FOLLOWINGS_SUCCESS: {
-      return {
-        ...state,
-        followingList: action.data
-      };
-    }
-    case LOAD_FOLLOWINGS_FAILURE: {
-      return {
-        ...state
-      };
-    }
-    case REMOVE_FOLLOWER_REQUEST: {
-      return {
-        ...state
-      };
-    }
-    case REMOVE_FOLLOWER_SUCCESS: {
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Followers: state.me.Followers.filter(v => v.id !== action.data)
-        },
-        followerList: state.followerList.filter(v => v.id !== action.data)
-      };
-    }
-    case REMOVE_FOLLOWER_FAILURE: {
-      return {
-        ...state
-      };
-    }
-
-    case EDIT_NICKNAME_REQUEST: {
-      return {
-        ...state,
-        isEditingNickname: true,
-        editNicknameErrorReason: ""
-      };
-    }
-    case EDIT_NICKNAME_SUCCESS: {
-      return {
-        ...state,
-        isEditingNickname: false,
-        me: {
-          ...state.me,
-          nickname: action.data
-        }
-      };
-    }
-    case EDIT_NICKNAME_FAILURE: {
-      return {
-        ...state,
-        isEditingNickname: false,
-        editNicknameErrorReason: action.error
-      };
-    }
-
-    case REMOVE_POST_OF_ME: {
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Posts: state.me.Posts.filter(v => v.id !== action.data)
-        }
-      };
-    }
-    default: {
-      return {
-        ...state
-      };
-    }
-  }
+  });
 };
